6b43cf2b8def1ad1ce5c86e16e346a1ed70abd15
Nicolas BITAN <nicolas.bitan@igtp3-mac05.isim.intra>
2017-03-02 12:39:21 +0100

    Connexion avancée + ajout d'un utilisateur possible

--- Facetech/ViewController.swift
+++ Facetech/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
@@ -24,7 +25,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     var mdp : String = "";
     
     
-    //MARK: UIViewController methods
+    
+    
+    //MARK: - UIViewController methods
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -42,8 +45,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 
     
-    //MARK: UITextFieldDelegate
     
+    
+    
+    //MARK: - UITextFieldDelegate
+    
+    /// <#Description#>
+    ///
+    /// - Parameter textField: <#textField description#>
+    /// - Returns: <#return value description#>
     func textFieldShouldReturn(_ textField: UITextField) -> Bool
     {
         identifiantTextField.resignFirstResponder();
@@ -51,6 +61,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return true;
     }
     
+    /// <#Description#>
+    ///
+    /// - Parameter textField: <#textField description#>
     func textFieldDidEndEditing(_ textField: UITextField)
     {
         if  let saisieId = self.identifiantTextField.text
@@ -65,18 +78,85 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     
     
-    //MARK: Functions
+    //MARK: - Navigation
     
     
-    override func shouldPerformSegue(withIdentifier identifier: String?, sender: Any?) -> Bool {
-            if self.id == "Nicolas" && self.mdp == "123456"
+    /// <#Description#>
+    ///
+    /// - Parameters:
+    ///   - identifier: <#identifier description#>
+    ///   - sender: <#sender description#>
+    /// - Returns: <#return value description#>
+    override func shouldPerformSegue(withIdentifier identifier: String?, sender: Any?) -> Bool
+    {
+        guard let context = self.getContext(errorMsg: "Could not load data") else {return false}
+        
+        var user : [Utilisateur] = []
+        
+        let request : NSFetchRequest<Utilisateur> = Utilisateur.fetchRequest()
+        do{
+            try user = context.fetch(request)
+        }
+        catch let error as NSError{
+            self.alert(error: error)
+        }
+        for people in user
+        {
+            if people.adresseMail == self.id && people.motDePasse == self.mdp
             {
                 messageErreurLabel.isHidden = true;
                 return true
             }
-            messageErreurLabel.isHidden = false;
-            return false
+        }
+        messageErreurLabel.isHidden = false;
+        return false
     }
+    
+    
+    
+    
+    
+    //MARK: - Helper methods
+    
+    /// <#Description#>
+    ///
+    /// - Parameters:
+    ///   - errorMsg: <#errorMsg description#>
+    ///   - userInfoMsg: <#userInfoMsg description#>
+    /// - Returns: <#return value description#>
+    func getContext(errorMsg: String, userInfoMsg: String = "could notr eretrieve data context") -> NSManagedObjectContext?
+    {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+            self.alert(WithTitle:errorMsg, andMessage: userInfoMsg)
+            return nil
+        }
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    
+    /// <#Description#>
+    ///
+    /// - Parameters:
+    ///   - title: <#title description#>
+    ///   - msg: <#msg description#>
+    func alert(WithTitle title: String, andMessage msg: String = "")
+    {
+        let alert = UIAlertController(title: title,
+                                      message: msg,
+                                      preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(cancelAction)
+        present(alert, animated:true)
+    }
+    
+    
+    /// <#Description#>
+    ///
+    /// - Parameter error: <#error description#>
+    func alert(error : NSError){
+        self.alert(WithTitle: "\(error)", andMessage: "\(error.userInfo)")
+    }
+    
     
 
 }
---
+++ Utilisateur.swift
@@ -0,0 +1,16 @@
+//
+//  Utilisateur.swift
+//  Facetech
+//
+//  Created by Nicolas BITAN on 02/03/2017.
+//  Copyright © 2017 Nicolas BITAN. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Utilisateur
+{
+
+}
