a0df804e619e30a15a1b0d5cbad44de4b14993fc
Nicolas BITAN <nicolas.bitan@igtp3-mac04.isim.intra>
2017-03-15 11:11:46 +0100

    MàJ SetModel

---
+++ Facetech/AnneesSetModel.swift
@@ -0,0 +1,42 @@
+//
+//  AnneesSetModel.swift
+//  Facetech
+//
+//  Created by Nicolas BITAN on 08/03/2017.
+//  Copyright © 2017 Nicolas BITAN. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class AnneesSetModel : NSObject{
+    
+    static var anneesSet = {
+        return AnneesSetModel()
+    }()
+    
+    private static var touteLesAnnees : [AnneePromo] = []
+    
+    
+    
+    class func getToutesLesAnnees() throws -> [AnneePromo]
+    {
+        if (touteLesAnnees.count == 0)
+        {
+            let context = CoreDataManager.context
+        
+            let request : NSFetchRequest<AnneePromo> = AnneePromo.fetchRequest()
+        
+            do
+            {
+                try touteLesAnnees = context.fetch(request)
+            }
+            catch let error as NSError{
+                throw error
+            }
+        }
+        
+        return touteLesAnnees
+    }
+}
+    
