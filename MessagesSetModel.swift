cb9443b6907d0bf19e397de3ca0788587b9fc703
Nicolas BITAN <nicolas.bitan@igtp4-mac01.isim.intra>
2017-03-22 08:58:14 +0100

    Trying to debug

---
+++ MessagesSetModel.swift
@@ -0,0 +1,15 @@
+//
+//  MessagesSetModel.swift
+//  Facetech
+//
+//  Created by Nicolas BITAN on 22/03/2017.
+//  Copyright © 2017 Nicolas BITAN. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class MessagesSetModel: NSObject{
+
+}
